97f22f6869455a3fef10035ea7f0c8cc692c52f5
doduc <doduchieu.kstn@gmail.com>
2022-09-20 17:05:59 +0700

    Update ui home_screen.dart

--- lib/ui/home/home_screen.dart
+++ lib/ui/home/home_screen.dart
@@ -3,7 +3,7 @@ import 'package:deverse_host_app/ui/home/home_model.dart';
 import 'package:deverse_host_app/ui/session_manager/session_manager_screen.dart';
 import 'package:deverse_host_app/ui/widgets/console_view.dart';
 import 'package:fluent_ui/fluent_ui.dart';
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' hide Checkbox, Colors;
 import 'package:provider/provider.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -29,48 +29,75 @@ class _HomeScreenState extends State<HomeScreen> {
 
   Widget _buildTemplateList(List<SubWorldTemplate> templates) {
     return Column(
-        children: templates
-            .map((template) => RadioButton(
-                checked: _selectedTemplate?.id == template.id,
-                content: Text(template.display_name),
-                onChanged: (value) {
-                  setState(() {
-                    _selectedTemplate = template;
-                  });
-                }))
-            .toList());
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: templates
+          .map((template) => Checkbox(
+              checked: _selectedTemplate?.id == template.id,
+              content: Text(template.display_name),
+              onChanged: (value) {
+                setState(() {
+                  _selectedTemplate = template;
+                });
+              }))
+          .toList(),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return NavigationView(
-        content: Center(
+        content: Padding(
+      padding: const EdgeInsets.all(8.0),
       child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Consumer<HomeModel>(builder: (context, model, child) {
             if (model.isAuthenticated) {
               return Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  const Text("Select root subworld theme"),
-                  SizedBox(
-                    width: 200,
-                    child: Consumer<HomeModel>(
-                      builder: (context, model, child) {
-                        if (model.isAuthenticated) {
-                          return _buildTemplateList(model.templates);
-                        }
-                        return const Text("Please login first");
-                      },
-                    ),
+                  const Text(
+                    "Select root subworld theme",
+                    style: TextStyle(fontWeight: FontWeight.w500),
                   ),
-                  Button(
-                    onPressed: () {
-                      if (_selectedTemplate != null) {
-                        Navigator.of(context).push(MaterialPageRoute(builder: (context) => SessionManagerScreen(rootTemplate: _selectedTemplate!)));
-                      }
-                    },
-                    child: const Text("Next"),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Colors.grey, width: 0.1),
+                        color: Colors.white),
+                    padding: const EdgeInsets.all(8),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                          children: [
+                            Consumer<HomeModel>(
+                              builder: (context, model, child) {
+                                if (model.isAuthenticated) {
+                                  return _buildTemplateList(model.templates);
+                                }
+                                return const Text("Please login first");
+                              },
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          width: 100,
+                          child: FilledButton(
+                            onPressed: () {
+                              if (_selectedTemplate != null) {
+                                Navigator.of(context).push(MaterialPageRoute(
+                                    builder: (context) => SessionManagerScreen(
+                                        rootTemplate: _selectedTemplate!)));
+                              }
+                            },
+                            child: const Text("Next"),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ],
               );
@@ -96,6 +123,7 @@ class _HomeScreenState extends State<HomeScreen> {
                   child: const Text("Authenticate"));
             },
           ),
+          const Expanded(child: SizedBox()),
           const ConsoleView()
         ],
       ),
