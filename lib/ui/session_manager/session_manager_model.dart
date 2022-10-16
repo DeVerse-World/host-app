b9a53d2e5cefe6cc7ebbf827ddaee39e61575339
Nam Nguyen <nam.nt1@teko.vn>
2022-10-16 16:53:26 +0700

    Update app icons

--- lib/ui/session_manager/session_manager_model.dart
+++ lib/ui/session_manager/session_manager_model.dart
@@ -40,32 +40,6 @@ class SessionManagerModel extends BaseModel {
       savedConfigs = value;
       notifyListeners();
     });
-    // savedConfigs = [
-    //   SubWorldConfig(1, SubWorldTheme(1, "Blizzard", "", "", "", 0, 0), "Nam",
-    //       8, 7777, 7877),
-    //   SubWorldConfig(2, SubWorldTheme(2, "Inferno", "", "", "", 0, 0), "Hieu",
-    //       18, 7777, 7877),
-    //   SubWorldConfig(3, SubWorldTheme(3, "Dungeon", "", "", "", 0, 0), "Thuan",
-    //       32, 7777, 7877),
-    //   SubWorldConfig(4, SubWorldTheme(4, "Dungeon", "", "", "", 0, 0), "Thuan",
-    //       32, 7777, 7877),
-    //   SubWorldConfig(5, SubWorldTheme(5, "Dungeon", "", "", "", 0, 0), "Thuan",
-    //       32, 7777, 7877),
-    //   SubWorldConfig(6, SubWorldTheme(6, "Blizzard", "", "", "", 0, 0), "Nam",
-    //       8, 7777, 7877),
-    //   SubWorldConfig(7, SubWorldTheme(7, "Inferno", "", "", "", 0, 0), "Hieu",
-    //       18, 7777, 7877),
-    //   SubWorldConfig(8, SubWorldTheme(8, "Dungeon", "", "", "", 0, 0), "Thuan",
-    //       32, 7777, 7877),
-    //   SubWorldConfig(9, SubWorldTheme(9, "Blizzard", "", "", "", 0, 0), "Nam",
-    //       8, 7777, 7877),
-    //   SubWorldConfig(10, SubWorldTheme(10, "Inferno", "", "", "", 0, 0), "Hieu",
-    //       18, 7777, 7877),
-    //   SubWorldConfig(11, SubWorldTheme(11, "Dungeon", "", "", "", 0, 0),
-    //       "Thuan", 32, 7777, 7877),
-    //   SubWorldConfig(12, SubWorldTheme(12, "Dungeon", "", "", "", 0, 0),
-    //       "Thuan", 32, 7777, 7877),
-    // ];
   }
 
   void onSelectTemplate(SubWorldTemplate template) {
